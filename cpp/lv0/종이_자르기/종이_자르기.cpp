14d9b97a0eec8a7a3a329cdbcf10a33c75a086c4
96sunghyun <visioner2168@gmail.com>
2022-12-08 20:07:17 +0900

    cpp algorithm move floder

---
+++ cpp/lv0/종이_자르기/종이_자르기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 이게 머지.. .. .......
+int solution(int M, int N) {
+    int answer = 0;
+    answer = M * N - 1;
+    return answer;
+}
