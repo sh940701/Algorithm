5046dcc5c34efb988dece7928ca4cbd45188b258
96sunghyun <visioner2168@gmail.com>
2022-12-10 02:27:31 +0900

    cpp algorithm 세균 증식

--- cpp/lv0/세균_증식/세균_증식.cpp
+++ cpp/lv0/세균_증식/세균_증식.cpp
@@ -1,13 +1,9 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
-// 이게 머야..
 using namespace std;
-
+// 이게머고..
 int solution(int n, int t) {
-    // int answer = 1;
-    for (int i = 0; i < t; ++i) {
-        n *= 2;
-    }
-    return n;
+    return n * std::pow(2, t);
 }
